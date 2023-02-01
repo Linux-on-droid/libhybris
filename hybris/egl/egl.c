3a49f62dc52f593c9af1a4e46184e1d78397a2bd
Eugenio Paolantonio (g7) <me@medesimo.eu>
2023-02-01 01:46:50 +0100

    hybris: egl: override eglSwapBufferWithDamageKHR as well
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- hybris/egl/egl.c
+++ hybris/egl/egl.c
@@ -633,6 +633,7 @@ struct FuncNamePair {
 static struct FuncNamePair _eglHybrisOverrideFunctions[] = {
 	OVERRIDE_MY(eglCreateImageKHR),
 	OVERRIDE_MY(eglSwapBuffersWithDamageEXT),
+	OVERRIDE_TO(eglSwapBuffersWithDamageKHR, _my_eglSwapBuffersWithDamageEXT),
 	OVERRIDE_MY(glEGLImageTargetTexture2DOES),
 	OVERRIDE_MY(eglDestroyImageKHR),
 	OVERRIDE_SAMENAME(eglGetError),
