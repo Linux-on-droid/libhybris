36934a2cae65f0f8e4042c9d268730d477fefa87
luka177 <lukapanio@gmail.com>
2026-01-25 16:48:08 +0000

    hybris: egl: glvnd: Advertise EGL_EXT_image_dma_buf_import

--- hybris/egl/glvnd/eglglvnd.cpp
+++ hybris/egl/glvnd/eglglvnd.cpp
@@ -102,7 +102,7 @@ __eglGLVNDGetVendorString(int name)
             " EGL_EXT_platform_wayland EGL_KHR_platform_wayland"
 #endif
 #ifdef WANT_LINDROID_DRM_GLOBAL
-	    " EGL_KHR_platform_gbm EGL_MESA_platform_gbm EGL_EXT_platform_base"
+	    " EGL_KHR_platform_gbm EGL_MESA_platform_gbm EGL_EXT_platform_base EGL_EXT_image_dma_buf_import"
 #endif
             ;
     }
